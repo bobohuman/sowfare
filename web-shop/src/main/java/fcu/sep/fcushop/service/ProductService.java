75352b912b4e5068c123e4d866a00f9cb61ca205
bobo <pauleric789456@gmail.com>
2021-12-13 06:31:08 +0800

    databasecreate

--- web-shop/src/main/java/fcu/sep/fcushop/service/ProductService.java
+++ web-shop/src/main/java/fcu/sep/fcushop/service/ProductService.java
@@ -21,7 +21,7 @@ public class ProductService {
   public List<Product> getProducts() {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
       String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
-      + " from PRODUCT ";
+      + " from product ";
       return connection.createQuery(query).executeAndFetch(Product.class);
     }
   }
@@ -29,7 +29,7 @@ public class ProductService {
   public List<Product> getProducts(String keyword) {
     try (Connection connection = sql2oDbHandler.getConnector().open()) {
       String query = "select ID id, NAME name, IMAGE_URL imageUrl, PRICE price, DESCRIPTION description"
-      + " from PRODUCT where name like :keyword";
+      + " from product where name like :keyword";
 
       return connection.createQuery(query)
       .addParameter("keyword", "%" + keyword + "%")

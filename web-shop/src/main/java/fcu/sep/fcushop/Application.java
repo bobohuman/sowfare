629c9478713772a641d220ce48eb8af43fe0ca3a
bobo <pauleric789456@gmail.com>
2021-12-12 17:21:02 +0800

    update

---
+++ web-shop/src/main/java/fcu/sep/fcushop/Application.java
@@ -0,0 +1,16 @@
+package fcu.sep.fcushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * use this for home.
+ *
+ * @SpringBootApplication.
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}

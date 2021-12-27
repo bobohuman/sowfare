544b9430343a10719f293f7fd8ebd4a3bdfb55fe
wayne881214 <77848460+wayne881214@users.noreply.github.com>
2021-12-27 20:42:47 +0800

    123

--- web-shop/src/main/java/fcu/sep/fcushop/controller/UserController.java
+++ web-shop/src/main/java/fcu/sep/fcushop/controller/UserController.java
@@ -1,4 +1,10 @@
 package fcu.sep.fcushop.controller;
 
+import fcu.sep.fcushop.model.User;
+import fcu.sep.fcushop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 }

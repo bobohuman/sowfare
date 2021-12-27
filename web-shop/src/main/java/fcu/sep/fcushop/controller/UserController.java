67723f1cb23f0a4d9d8e2862da05ed346a7049f6
wayne881214 <77848460+wayne881214@users.noreply.github.com>
2021-12-27 21:24:56 +0800

    123

--- web-shop/src/main/java/fcu/sep/fcushop/controller/UserController.java
+++ web-shop/src/main/java/fcu/sep/fcushop/controller/UserController.java
@@ -6,5 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
 public class UserController {
+  @Autowired
+  UserService userManager;
+
+  @GetMapping("/users")
+  public List<User> getUsers() {
+    System.out.println("-------------------------------------------");
+    System.out.println( userManager.getUsers());
+    System.out.println("-------------------------------------------");
+
+    return userManager.getUsers();
+
+  }
+
+  @GetMapping("/users/{password}")
+  public List<User> getUsers(@PathVariable("password") String password) {
+    return userManager.getUsers(password);
+  }
+
+  @GetMapping("/users/{account}/{password}")
+  public List<User> getUsers(@PathVariable("account") String account,@PathVariable("password") String password) {
+    return userManager.getUsers(account,password);
+  }
 }

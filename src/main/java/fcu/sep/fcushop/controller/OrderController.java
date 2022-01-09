26b152293c3d164ba968a177f2fdb8500b7f2dc8
bobo <pauleric789456@gmail.com>
2022-01-09 21:25:10 +0800

    chckstyle

--- src/main/java/fcu/sep/fcushop/controller/OrderController.java
+++ src/main/java/fcu/sep/fcushop/controller/OrderController.java
@@ -2,11 +2,12 @@ package fcu.sep.fcushop.controller;
 
 import fcu.sep.fcushop.model.Order;
 import fcu.sep.fcushop.service.OrderService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+
 
 /**
  *RestController.
@@ -31,28 +32,37 @@ public class OrderController {
 
 
   @GetMapping("/orders/{userid}/{state}")
-  public List<Order> getOrders(@PathVariable("userid") int userid,@PathVariable("state") int state) {
-    return orderManager.getOrders(userid,state);
+  public List<Order> getOrders(@PathVariable("userid") int userid,
+                               @PathVariable("state") int state) {
+    return orderManager.getOrders(userid, state);
   }
+
   @GetMapping("/sellerorder/{proid}/{state}")
-  public List<Order> sellerorder(@PathVariable("proid") int proid,@PathVariable("state") int state) {
-    return orderManager.getsellers(proid,state);
+  public List<Order> sellerorder(@PathVariable("proid") int proid,
+                                 @PathVariable("state") int state) {
+    return orderManager.getsellers(proid, state);
   }
 
   @GetMapping("/orders/{proid}/{procount}/{protime}/{uid}/{state}")
-  public String addProducts(@PathVariable("proid") int proid, @PathVariable("procount") int procount,
-   @PathVariable("protime") int protime, @PathVariable("uid") int uid, @PathVariable("state") int state) {
-    return orderManager.addOrders(proid,procount,protime,uid,state);
+  public String addProducts(@PathVariable("proid") int proid,
+                            @PathVariable("procount")  int procount,
+                            @PathVariable("protime") int protime,
+                            @PathVariable("uid") int uid,
+                            @PathVariable("state") int state) {
+    return orderManager.addOrders(proid, procount, protime, uid, state);
   }
 
   @GetMapping("/orderdelte/{proid}/{uid}/{state}")
-  public String  getOrders(@PathVariable("proid") int proid, @PathVariable("uid") int uid, @PathVariable("state") int state) {
-    return orderManager.deleteOrders(proid,uid,state);
+  public String  getStrings(@PathVariable("proid") int proid,
+                            @PathVariable("uid") int uid,
+                            @PathVariable("state") int state) {
+    return orderManager.deleteOrders(proid, uid, state);
   }
 
   @GetMapping("/orderupdate/{uid}/{state}")
-  public String  getStrings(@PathVariable("uid") int uid, @PathVariable("state") int state) {
-    return orderManager.updateOrders(uid,state);
+  public String  getStrings(@PathVariable("uid") int uid,
+                            @PathVariable("state") int state) {
+    return orderManager.updateOrders(uid, state);
   }
 
   @GetMapping("/orderupdate/{id}")
--- src/main/java/fcu/sep/fcushop/controller/UserController.java
+++ src/main/java/fcu/sep/fcushop/controller/UserController.java
@@ -3,22 +3,28 @@ package fcu.sep.fcushop.controller;
 
 import fcu.sep.fcushop.model.User;
 import fcu.sep.fcushop.service.UserService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+/**
+ *GetMapping.
+ */
 
 @RestController
 public class UserController {
   @Autowired
   UserService userManager;
+  /**
+   *GetMapping.
+   */
 
   @GetMapping("/users")
   public List<User> getUsers() {
     System.out.println("-------------------------------------------");
-    System.out.println( userManager.getUsers());
+    System.out.println(userManager.getUsers());
     System.out.println("-------------------------------------------");
 
     return userManager.getUsers();
@@ -27,16 +33,21 @@ public class UserController {
 
 
   @GetMapping("/users/{account}/{password}")
-  public List<User> getUsers(@PathVariable("account") String account,@PathVariable("password") String password) {
-    return userManager.getUsers(account,password);
+  public List<User> getUsers(@PathVariable("account") String account,
+      @PathVariable("password") String password) {
+    return userManager.getUsers(account, password);
   }
+
   @GetMapping("/login.html/users/{account}")
   public List<User> getUsers(@PathVariable("account") String account) {
     return userManager.getUsers(account);
   }
+
   @GetMapping("/users/{name}/{gmail}/{account}/{password}")
-  public String getUsers(@PathVariable("name") String name, @PathVariable("gmail") String gmail,
-                         @PathVariable("account") String account, @PathVariable("password") String password) {
-    return userManager.getUsers(name,gmail,account,password);
+  public String getUsers(@PathVariable("name") String name,
+                         @PathVariable("gmail") String gmail,
+                         @PathVariable("account") String account,
+                         @PathVariable("password") String password) {
+    return userManager.getUsers(name, gmail, account, password);
   }
 }
